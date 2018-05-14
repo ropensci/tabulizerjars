d2e39a093f369dc15ae67485ba0b11bb59311b34
Tom Paskhalis <tpaskhalis@gmail.com>
2018-05-14 12:18:45 +0100

    Autogenerate package help page

--- R/tabulizerjars.R
+++ R/tabulizerjars.R
@@ -1,15 +1,16 @@
 #' @docType package
 #' @name tabulizerjars
-#' @title Java '.jar' files for 'tabulizer'
-#' @description The only purpose of 'tabulizerjars' is to distribute releases of 
-#' 'Tabula' Java library to used with 'tabulizer' package. Note that the package
-#' itself does not provide any functionality apart from basic linking to Java
-#' version installed on the system.
+#' @title Java '.jar' files of 'Tabula' PDF table extractor library.
+#' @description 'Tabula' is a Java library designed to computationally extract
+#' tables from PDF documents. The only purpose of 'tabulizerjars' is to
+#' distribute releases of 'Tabula' Java library to be used with 'tabulizer' package. 
+#' Note that the package itself does not provide any functionality apart from 
+#' basic linking to Java version installed on the system.
 #' 
 #' @examples 
 #' library("tabulizerjars")
 #' 
 #' @references See \href{https://tabula.technology/}{Tabula} webpage and 
-#' \href{https://github.com/ropensci/tabulizer/]{tabulizer} package for 
+#' \href{https://github.com/ropensci/tabulizer/}{tabulizer} package for 
 #' more details and all the features.
 NULL
