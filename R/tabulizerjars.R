a71be499d3c3bc5823d35a47ac8da1dc5613eaf9
Tom Paskhalis <tpaskhalis@gmail.com>
2018-05-14 11:56:47 +0100

    Add package help page

---
+++ R/tabulizerjars.R
@@ -0,0 +1,15 @@
+#' @docType package
+#' @name tabulizerjars
+#' @title Java '.jar' files for 'tabulizer'
+#' @description The only purpose of 'tabulizerjars' is to distribute releases of 
+#' 'Tabula' Java library to used with 'tabulizer' package. Note that the package
+#' itself does not provide any functionality apart from basic linking to Java
+#' version installed on the system.
+#' 
+#' @examples 
+#' library("tabulizerjars")
+#' 
+#' @references See \href{https://tabula.technology/}{Tabula} webpage and 
+#' \href{https://github.com/ropensci/tabulizer/]{tabulizer} package for 
+#' more details and all the features.
+NULL
